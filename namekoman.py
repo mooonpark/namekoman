bccc22fb8344d514230583aa34c0e9d4f075bbaa
Sherman Liu <sherman.liu@student.com>
2020-11-21 09:28:53 +0800

    U update comment

--- namekoman.py
+++ namekoman.py
@@ -9,9 +9,9 @@ import collections
 
 from nameko.cli.utils.config import setup_config
 from nameko.standalone.rpc import ClusterRpcClient
+
 from PyQt5.Qt import QStandardItem, QStandardItemModel, QPoint, QCursor
 from PyQt5.QtCore import Qt as QtCoreQt
-
 from PyQt5.QtGui import QMouseEvent
 from PyQt5.Qsci import QsciScintilla, QsciLexerJSON
 from PyQt5.QtWidgets import (QWidget, QTreeView, QPushButton, QLineEdit, QPlainTextEdit,
@@ -30,46 +30,35 @@ CONST_PARAMS = "params"
 CONST_BROKER = "amqp://guest:guest@localhost"
 CONST_SERVICE_INPUT = "properties"
 CONST_METHOD_INPUT = "page_bed_status"
-
 AMQP_URI_CONFIG_KEY = "AMQP_URI"
 
-README = """
-1. 解压namekoman.zip将namekoman放到/Applications目录下，双击打开，如遇权限问题，系统偏好设置->安全性与隐私->通用，点击允许打开
-2. namekoman类似于postman，是为了解决使用nameko shell发送请求麻烦的问题，只有mac版。namekoman可以添加service，
-添加method和params，点击发送按钮可以发送rpc请求。编辑过程中数据会被写进namekoman.json，保存在磁盘上。
-3. 在做完第一步之后，可以进入/Applications目录找到namekoman，右键选择显示包内容，进入Contents/Resources，
-找到namekoman.json，可以对其进行编辑，以快速导入请求数据
-4. namekoman成功发送成功请求需依赖mq，需配置broker，默认broker：amqp://guest:guest@localhost
-5. rpc超时时间设置为了5s，在点击发送按钮后如果鼠标指针出现转圈，请等待5s
-6. 编辑params过程中，按下cmd+r，会有惊喜
-7. 代码地址：https://github.com/Liusple/namekoman.git
-8. 新建的service和method不建议输入中文，也不应该输入中文，可能会导致程序异常（这条待定）
-9. 有建议或有bug可以向我反馈
-10.TODO：
-    1）app体积太大
-    2）支持一个service下新建多个同名method
-    3）多个发送请求页
-11. 感谢
-"""
+
+def readFile(filePath):
+    with open(filePath) as f:
+        return f.read()
+
+
+README = readFile("README.md")
 
 
 def getFilePath(filepath):
+    # 需要这样写，否则pyinstaller打包后会找不到文件
     return os.path.join(os.path.dirname(sys.argv[0]), filepath)
 
 
-# 需要这样写，否则pyinstaller打包后会找不到文件
 CONST_DATA_FILE_PATH = getFilePath("namekoman.json")
-# CONST_FOLDER_PATH = getFilePath("icons8-folder.svg")
-# CONST_FILE_PATH = getFilePath("icons8-file.svg")
 
 
 def getAMQPConfig(broker):
-    # nameko mq配置key使用的AMQP_URI_CONFIG_KEY
     return {AMQP_URI_CONFIG_KEY: broker}
 
 
 def dictToJsonStr(dataDict):
-    return json.dumps(dataDict, sort_keys=True, indent=4, separators=(", ", ": "), ensure_ascii=False)
+    return json.dumps(dataDict,
+                      sort_keys=True, indent=4,
+                      separators=(", ", ": "),
+                      ensure_ascii=False
+                      )
 
 
 def strToJsonStr(dictStr):
@@ -163,7 +152,7 @@ class Storage(object):
             return collections.OrderedDict()
 
 
-STORAGE = Storage(CONST_DATA_FILE_PATH)
+storage = Storage(CONST_DATA_FILE_PATH)
 
 
 class TreeNode(QStandardItem):
@@ -191,18 +180,12 @@ class TreeNode(QStandardItem):
         else:
             self.setMethodName("")
 
-        # if CONST_PARAMS in kwargs:
-        #     self.params = kwargs[CONST_PARAMS]
-        #     del kwargs[CONST_PARAMS]
-        # else:
-        #     self.params = ""
-
         super().__init__(*args, **kwargs)
 
-    def setIsDir(self, isDir):
-        self.isDir = isDir
+    def setIsDir(self, value):
+        self.isDir = value
 
-    def checkIsDir(self):
+    def getIsDir(self):
         return self.isDir
 
     def getServiceName(self):
@@ -218,14 +201,14 @@ class TreeNode(QStandardItem):
         self.method = method
 
     def getName(self):
-        if self.checkIsDir():
+        if self.getIsDir():
             return self.service
         return self.method
 
     def updateName(self, name):
 
-        if self.checkIsDir():
-            flag = STORAGE.updateServiceName(self.service, name)
+        if self.getIsDir():
+            flag = storage.updateServiceName(self.service, name)
             if not flag:
                 return False
             self.setServiceName(name)
@@ -238,7 +221,7 @@ class TreeNode(QStandardItem):
                 childNode.setServiceName(name)
                 index += 1
         else:
-            flag = STORAGE.updateMethodName(self.service, self.method, name)
+            flag = storage.updateMethodName(self.service, self.method, name)
             if not flag:
                 return False
             self.setMethodName(name)
@@ -247,11 +230,10 @@ class TreeNode(QStandardItem):
         return True
 
     def getRpcInfo(self):
-        params = STORAGE.getParam(self.service, self.method)
         return {
             CONST_SERVICE: self.service,
             CONST_METHOD: self.method,
-            CONST_PARAMS: params,
+            CONST_PARAMS: storage.getParam(self.service, self.method),
         }
 
 
@@ -267,8 +249,6 @@ class FolderTreeView(QTreeView):
         # self.setAcceptDrops(False)
         # self.setDropIndicatorShown(False)
         # self.setFrameStyle(QFrame.NoFrame)
-        # self.folderIcon = QIcon(CONST_FOLDER_PATH)
-        # self.fileIcon = QIcon(CONST_FILE_PATH)
 
     # 禁用双击事件
     def mouseDoubleClickEvent(self, e: QMouseEvent) -> None:
@@ -281,16 +261,17 @@ class FolderTreeView(QTreeView):
         return self.model().itemFromIndex(self.indexAt(pos))
 
 
-class FolderBar(QWidget):
+class FolderWidget(QWidget):
 
-    # 传入三个edit控件是不是不好，是不是应该用信号进行文本的更新？
     def __init__(self, serviceEdit, methodEdit, paramsEdit):
+        """
+        传入三个edit控件是不是不好，是不是应该用信号进行文本的更新？
+        """
         super().__init__()
         self.setMinimumSize(300, 400)
         self.setMaximumSize(400, 2000)
         layout = QBoxLayout(QBoxLayout.TopToBottom)
-        # 取消组件间间隙
-        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setContentsMargins(0, 0, 0, 0)  # 取消组件间间隙
         self.treeView = FolderTreeView()
         layout.addWidget(self.treeView)
         self.loadFromFile()
@@ -298,14 +279,20 @@ class FolderBar(QWidget):
         self.treeView.clicked.connect(self.onTreeNodeClicked)
         self.treeView.customContextMenuRequested.connect(self.showContextMenu)
 
-        # 鼠标点击的item
+        # 鼠标选中的item
         self.clickedItem = None
         self.serviceEdit = serviceEdit
         self.methodEdit = methodEdit
         self.paramsEdit = paramsEdit
 
+    def showInfo(self, service, method, params):
+        self.serviceEdit.setText(service)
+        self.methodEdit.setText(method)
+        if params:
+            self.paramsEdit.setText(params)
+
     def loadFromFile(self):
-        data = STORAGE.getData()
+        data = storage.getData()
         if data:
             for service, serviceDict in data.items():
                 node = self.newServiceNode(service)
@@ -318,9 +305,6 @@ class FolderBar(QWidget):
         node = TreeNode(name, **kwargs)
         if not isDir:
             node.setDropEnabled(False)
-            # node.setIcon(self.treeView.fileIcon)
-        # else:
-        #     node.setIcon(self.treeView.folderIcon)
 
         node.setText(name)
         parent.appendRow(node)
@@ -334,7 +318,7 @@ class FolderBar(QWidget):
             action = menu.addAction("add service")
             action.triggered.connect(self.onAddService)
         if self.clickedItem is not None:
-            if self.clickedItem.checkIsDir():
+            if self.clickedItem.getIsDir():
                 action = menu.addAction("add method")
                 action.triggered.connect(self.onAddMethod)
             action = menu.addAction("rename")
@@ -344,12 +328,10 @@ class FolderBar(QWidget):
 
     def newServiceNode(self, service: str) -> TreeNode:
         node = TreeNode(service, service=service, method=service, isDir=True)
-        # node.setIcon(self.treeView.folderIcon)
         return node
 
     def newMethodNode(self, service, method: str) -> TreeNode:
         node = TreeNode(method, service=service, method=method, isDir=False)
-        # node.setIcon(self.treeView.fileIcon)
         return node
 
     def onTreeNodeClicked(self, modelIndex):
@@ -361,12 +343,6 @@ class FolderBar(QWidget):
         params = info.get(CONST_PARAMS, {})
         self.showInfo(service, method, dictToJsonStr(params))
 
-    def showInfo(self, service, method, params):
-        self.serviceEdit.setText(service)
-        self.methodEdit.setText(method)
-        if params:
-            self.paramsEdit.setText(params)
-
     def onAddService(self):
         name, ok = QInputDialog.getText(self, "⌨️", "Please enter service name")
         if ok:
@@ -377,7 +353,7 @@ class FolderBar(QWidget):
             if self.clickedItem is None:
                 self.treeView.addRootItem(node)
 
-            STORAGE.addService(name)
+            storage.addService(name)
 
     def onAddMethod(self):
         name, ok = QInputDialog.getText(self, "⌨️", "Please enter method name")
@@ -387,12 +363,12 @@ class FolderBar(QWidget):
                 return
             service = self.clickedItem.getName()
             node = self.newMethodNode(service, name)
-            if self.clickedItem.checkIsDir():
+            if self.clickedItem.getIsDir():
                 self.clickedItem.appendRow(node)
-            STORAGE.addMethod(service, name, None)
+            storage.addMethod(service, name, None)
 
     def onRename(self):
-        msg = "Please enter service name" if self.clickedItem.checkIsDir() else "Please enter method name"
+        msg = "Please enter service name" if self.clickedItem.getIsDir() else "Please enter method name"
         name, ok = QInputDialog().getText(self, "⌨️", msg, text=self.clickedItem.getName())
         if ok:
             if name == "":
@@ -402,7 +378,7 @@ class FolderBar(QWidget):
             if not self.clickedItem.updateName(name):
                 alert("Input has one, please change one!")
                 return
-            STORAGE.save()
+            storage.save()
             self.showInfo(self.clickedItem.getServiceName(), name, None)
 
 
@@ -411,7 +387,9 @@ class NamekoManQsciScintilla(QsciScintilla):
         super().__init__()
 
     def keyPressEvent(self, event):
-        # 监听cmd + r 按键事件
+        """
+        监听cmd + r 按键事件
+        """
         if event.modifiers() == QtCoreQt.ControlModifier and event.key() == QtCoreQt.Key_R:
             self.setText(strToJsonStr(self.text()))
         else:
@@ -434,6 +412,8 @@ class NamekoManWidget(QWidget):
         self.broker = CONST_BROKER
         self.brokerEdit.setPlaceholderText("Input mq broker, for example: {}".format(CONST_BROKER))
         self.brokerEdit.setText(CONST_BROKER)
+
+        # 初始化输出日志组件
         self.logTextBox = QTextEditLogger(self)
         self.logTextBox.widget.setMinimumHeight(150)
         self.logTextBox.widget.setMaximumHeight(250)
@@ -443,8 +423,8 @@ class NamekoManWidget(QWidget):
 
         # 初始化代码编辑控件
         lexer = QsciLexerJSON()
-        # lexer.setHighlightComments(False)
-        # lexer.setHighlightEscapeSequences(False)
+        lexer.setHighlightComments(False)
+        lexer.setHighlightEscapeSequences(False)
         self.paramsEdit = NamekoManQsciScintilla()
         self.paramsEdit.setLexer(lexer)
         self.paramsEdit.setUtf8(True)
@@ -469,12 +449,10 @@ class NamekoManWidget(QWidget):
         self.scrollArea.setWidget(self.resultLabel)
         self.scrollArea.setMinimumSize(500, 600)
 
-        self.serviceLabel = QLabel(CONST_SERVICE)
-        self.methodLabel = QLabel(CONST_METHOD)
-        self.paramsLabel = QLabel(CONST_PARAMS)
-        self.folderBar = FolderBar(self.serviceEdit, self.methodEdit, self.paramsEdit)
+        # 初始化文件夹树
+        self.folderBar = FolderWidget(self.serviceEdit, self.methodEdit, self.paramsEdit)
 
-        # 使用网格布局
+        # 使用网格布局进行布局
         self.layout = QGridLayout()
         self.layout.setContentsMargins(10, 0, 10, 0)
         self.layout.addWidget(self.folderBar, 0, 0, 25, 1)
@@ -491,20 +469,31 @@ class NamekoManWidget(QWidget):
 
         self.initNameko()
 
+    def showResult(self, result):
+        self.resultLabel.setText(result)
+        self.resultLabel.repaint()
+
+    def isHasNamekoClient(self):
+        return hasattr(self, self.RPC)
+
     def setNameko(self):
-        # 更新配置，再初始化一个新的rpc客户端
+        """
+        更新配置，再设置一个新的rpc客户端
+        """
         setup_config(None, define=getAMQPConfig(self.broker))
         client = ClusterRpcClient(timeout=5)
         setattr(self, self.RPC, client.start())
 
     def initNameko(self):
+        """
+        如果没有构建好rpc客户端，新建一个，如果已经构建好更新一下
+        """
         brokerEdit = self.brokerEdit.text()
         try:
-            if not hasattr(self, self.RPC):
+            if not self.isHasNamekoClient():
                 self.broker = brokerEdit
                 self.setNameko()
             else:
-                # 删除旧的rpc客户端，新建一个新的客户端
                 if brokerEdit != self.broker:
                     self.broker = brokerEdit
                     delattr(self, self.RPC)
@@ -513,14 +502,10 @@ class NamekoManWidget(QWidget):
             logging.exception(e)
             self.showResult(errorToJsonStr(repr(e)))
 
-    def showResult(self, result):
-        self.resultLabel.setText(result)
-        self.resultLabel.repaint()
-
     def onSendRpc(self):
         self.initNameko()
 
-        if not hasattr(self, self.RPC):
+        if not self.isHasNamekoClient():
             self.showResult(errorToJsonStr("Can't connect to mq, please check mq or broker!"))
             return
 
@@ -536,20 +521,20 @@ class NamekoManWidget(QWidget):
             return
         result = dictToJsonStr(result)
         self.showResult(result)
-        STORAGE.addMethod(service, method, params)
+        storage.addMethod(service, method, params)
         logging.info("Send rpc end:\n{}".format(result))
 
 
 def error_handler(etype, value, tb):
     msg = "".join(traceback.format_exception(etype, value, tb))
-    logging.info("Unknow exception: {}".format(msg))
+    logging.error("Unknow exception: {}".format(msg))
 
 
 if __name__ == "__main__":
     sys.excepthook = error_handler
     app = QApplication([])
     widget = NamekoManWidget()
-    widget.resize(1100, 800)
+    widget.resize(1200, 800)
     widget.setMaximumSize(5000, 2500)
     widget.show()
     sys.exit(app.exec_())
